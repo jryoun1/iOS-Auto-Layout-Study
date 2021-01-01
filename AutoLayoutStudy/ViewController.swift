51ac3058820f1b9cf09b27d6b380b1bf23022ae7
yeon <jryoun1@naver.com>
2021-01-01 19:22:28 +0900

    feat: 첫번째 View에 데이터 설정

--- AutoLayoutStudy/ViewController.swift
+++ AutoLayoutStudy/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private weak var settingTableView: UITableView!
+    
+    private let personalInformation = ["Sign in to your iPhone"]
+    private let setting = ["General", "Accessibility", "Privacy", "Display & Brightness"]
+    private let passwords = ["Passwords"]
+    private let applications = ["Safari", "News", "Maps", "Shortcuts", "Health", "Siri & Search", "Photos", "Game Center"]
+    private let developer = ["Developer"]
+    
+    private lazy var sections = [personalInformation, setting, passwords, applications, developer]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
