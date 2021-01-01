8fd06648c0275e969e73ea5888345e3ee2af109b
yeon <jryoun1@naver.com>
2021-01-01 22:57:17 +0900

    feat: ViewController UITableViewDataSource extension해서 구현

--- AutoLayoutStudy/ViewController.swift
+++ AutoLayoutStudy/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     @IBOutlet private weak var settingTableView: UITableView!
     
     private let personalInformation = ["Sign in to your iPhone"]
@@ -20,7 +20,55 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        settingTableView.dataSource = self
     }
 }
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        for index in 0..<sections.count {
+            if section == index {
+                return sections[index].count
+            }
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return .leastNormalMagnitude
+        }
+        else if section == 4{
+            return 40
+        }
+        else {
+            return 10
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = selectLoadCell(by: indexPath)
+        return cell
+    }
+    
+    func selectLoadCell(by indexPath: IndexPath) -> UITableViewCell {
+        switch (indexPath.section, indexPath.row) {
+        case (0, 0):
+            let personalInfoCell = settingTableView.dequeueReusableCell(withIdentifier: "PersonInfoCell", for: indexPath) as! PersonInfoTableViewCell
+            personalInfoCell.setLabel(data: sections[indexPath.section][indexPath.row])
+            return personalInfoCell
+        default:
+            let otherSettingCell = settingTableView.dequeueReusableCell(withIdentifier: "OtherSettingCell", for: indexPath) as! OtherSettingTableViewCell
+            otherSettingCell.accessoryType = .disclosureIndicator
+            otherSettingCell.setLabel(data: sections[indexPath.section][indexPath.row])
+            return otherSettingCell
+        }
+    }
+}
+
+
 
