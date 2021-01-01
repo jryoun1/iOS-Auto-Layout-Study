836d628bfcb5bf13d9714e4c30f82e2499888739
yeon <jryoun1@naver.com>
2021-01-01 20:41:57 +0900

    feat: 첫번째 view에서 personInfoCell 코드로 그려서 작성

---
+++ AutoLayoutStudy/OtherSettingTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  OtherSettingTableViewCell.swift
+//  AutoLayoutStudy
+//
+//  Created by Yeon on 2021/01/01.
+//
+
+import Foundation
+import UIKit
+
+final class OtherSettingTableViewCell: UITableViewCell {
+    
+}
