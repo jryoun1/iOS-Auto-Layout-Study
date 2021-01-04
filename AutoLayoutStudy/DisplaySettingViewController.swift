4972d4d545954808441b1bd0fa301ed48b236312
GREENOVER <humains29@gmail.com>
2021-01-04 12:58:26 +0900

    feat: 디스플레이 및 밝기 설정 화면 back 버튼명 변경

--- AutoLayoutStudy/DisplaySettingViewController.swift
+++ AutoLayoutStudy/DisplaySettingViewController.swift
@@ -4,6 +4,9 @@ class DisplaySettingViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationController?.isNavigationBarHidden = false
+        self.navigationController?.navigationBar.backItem?.title = "설정"
     }
 
 }
