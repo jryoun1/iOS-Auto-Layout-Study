795c5783b7bb07184d8cacde3edca20efbb72de0
zziro95 <opers7878@gmail.com>
2021-01-05 20:20:52 +0900

    feat: UITableView 구현

--- AutoLayoutStudy/ViewController.swift
+++ AutoLayoutStudy/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let tableView: UITableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        self.view.addSubview(self.tableView)
+        
+        // 제약조건을 설정해주기 위해서 꼭 false로 해줘야하는데 좀 더 자세히 알아보기
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        let tableViewTopAnchor = tableView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 0)
+        tableViewTopAnchor.isActive = true
+        let tableViewLeadingAnchor = tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 0)
+        tableViewLeadingAnchor.isActive = true
+        let tableViewTrailingAnchor = tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: 0)
+        tableViewTrailingAnchor.isActive = true
+        let tableViewBottomAnchor = tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: 0)
+        tableViewBottomAnchor.isActive = true
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+
+        cell.textLabel?.text = "\(indexPath.row) 번째 셀"
+
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
