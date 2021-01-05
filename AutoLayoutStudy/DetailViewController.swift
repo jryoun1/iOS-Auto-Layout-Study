ae315d2b05f17e8c4a38252f3faa9651a988799b
zziro95 <opers7878@gmail.com>
2021-01-06 00:27:22 +0900

    feat: DetailViewController 파일 추가

---
+++ AutoLayoutStudy/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  AutoLayoutStudy
+//
+//  Created by Jinho Choi on 2021/01/06.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
