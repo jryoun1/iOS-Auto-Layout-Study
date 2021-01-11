ec37c80afe5589f6403acb6d005d8efebdce9a4a
zziro95 <opers7878@gmail.com>
2021-01-12 00:57:47 +0900

    feat: 첫번째뷰 테이블뷰 만들고 셀 표시를 위한 더미 데이터 생성

--- AutoLayoutStudy/MainViewController.swift
+++ AutoLayoutStudy/MainViewController.swift
@@ -8,8 +8,35 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    let sectionHeader: [String] = ["ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ"]
+    let phoneNumberList: [[String]] = [["강아지", "개나리", "곰돌이"], ["나비", "나", "나침반", "나영석"], ["다치지말고", "두근두근"], ["라면", "러브", "라일락", "라디오", "레스고"], ["마음", "맴돌아", "매미", "메밀묵", "마차", "마징가", "물"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
+
+extension MainViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return phoneNumberList.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionHeader[section]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return phoneNumberList[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "MainViewCell", for: indexPath)
+        cell.textLabel?.text = "\(phoneNumberList[indexPath.section][indexPath.row])"
+        
+        return cell
+    }
+}
+
+extension MainViewController: UITableViewDelegate {
+    
 }
