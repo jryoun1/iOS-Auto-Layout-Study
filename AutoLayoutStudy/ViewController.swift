7a1a7ce97e30b47169e389db401e17c1ff86bb2b
odong-Tree <ss12357@naver.com>
2021-01-11 09:30:30 +0900

    feat: DetailView 스토리보드에 셀 2개 추가

--- AutoLayoutStudy/ViewController.swift
+++ AutoLayoutStudy/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
 }
-
