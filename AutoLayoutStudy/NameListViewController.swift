47f0a19d715b68d95157ffcb60f51c07f515fb28
GREENOVER <humains29@gmail.com>
2021-01-12 02:46:43 +0900

    feat: 연락처 두번째 화면 이미지,이름 구성

---
+++ AutoLayoutStudy/NameListViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class NameListViewController: UIViewController {
+
+    @IBOutlet weak var messageView: UIView!
+    @IBOutlet weak var callView: UIView!
+    @IBOutlet weak var facetimeView: UIView!
+    @IBOutlet weak var mmsView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
