a48ff4c17d77129d515da2aebed6471ed524063a
odong-Tree <ss12357@naver.com>
2021-01-12 01:38:38 +0900

    feat: 예제용 배열 추가

--- AutoLayoutStudy/ViewController.swift
+++ AutoLayoutStudy/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let firstLetterArray = ["ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    
+    private var people = [["고구마", "고랭지배추", "가나초콜릿"], ["나무", "노고지리", "누텔라", "네네치킨"], ["다다익선", "도롱뇽", "두부", "다이소"], ["라면", "리어카"], [], ["바이크", "복권", "비빔밥"], ["사랑", "상처", "수영장", "세탁소"], ["아저씨"], ["제이크", "제이콥", "준스"], ["차두리"], [], [], [], ["학교", "힌두교"], ["Alex", "AutoLayout"], ["B"], ["C"], [], [], [], ["G"], [], [], [], [], ["LL"], ["M"], ["N"], [], [], [], [], ["Star", "Strawberry", "Sing"], [], [], ["Vlog"], [], [], ["YG"], ["Zero"]]
 
     @IBOutlet weak var tableView: UITableView!
     
