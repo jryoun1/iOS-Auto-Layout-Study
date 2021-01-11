c28b9a2aa33e04b02eff7b962ae898c89d9fda4a
GREENOVER <humains29@gmail.com>
2021-01-12 03:15:23 +0900

    feat: 부가정보 둥근 테두리 적용

--- AutoLayoutStudy/NameListViewController.swift
+++ AutoLayoutStudy/NameListViewController.swift
@@ -6,13 +6,23 @@ class NameListViewController: UIViewController {
     @IBOutlet weak var callView: UIView!
     @IBOutlet weak var facetimeView: UIView!
     @IBOutlet weak var mmsView: UIView!
+    @IBOutlet weak var phoneNumberView: UIView!
+    @IBOutlet weak var memoView: UIView!
+    @IBOutlet weak var emergencyView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        makeRoundCorner()
+    }
+    
+    func makeRoundCorner() {
         messageView.layer.cornerRadius = 10
         callView.layer.cornerRadius = 10
         facetimeView.layer.cornerRadius = 10
         mmsView.layer.cornerRadius = 10
+        phoneNumberView.layer.cornerRadius = 10
+        memoView.layer.cornerRadius = 10
+        emergencyView.layer.cornerRadius = 10
     }
 }
