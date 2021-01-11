df220cdc3ebdfb8235e184d1df031f3716a84202
GREENOVER <humains29@gmail.com>
2021-01-11 19:51:49 +0900

    feat: ViewController 테이블뷰 데이터소스 및 딜리게이트 확장

--- AutoLayoutStudy/ViewController.swift
+++ AutoLayoutStudy/ViewController.swift
@@ -14,3 +14,15 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+
