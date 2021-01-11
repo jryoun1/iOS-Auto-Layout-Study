e34dd42585ff38ef918df856216f32391aca8014
odong-Tree <ss12357@naver.com>
2021-01-12 02:43:00 +0900

    DetailViewContorller에 UITablleViewDataSource 채택, 섹션, 셀 갯수 구현

--- AutoLayoutStudy/DetailViewController.swift
+++ AutoLayoutStudy/DetailViewController.swift
@@ -2,5 +2,41 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    let section2Title = ["메세지 보내기", "연락처 공유", "즐겨찾기에 추가"]
+    let section3Title = ["긴급 연락처에 추가"]
+    
+    @IBOutlet weak var detailTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+extension DetailViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 2: return 3
+        default: return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            
+        case 1:
+            
+        case 2:
+            
+        default:
+            
+        }
+    }
+    
     
 }
