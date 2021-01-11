930dad4e9c6d19560dcc3e638a5bf31d2851cf8e
zziro95 <opers7878@gmail.com>
2021-01-12 00:04:12 +0900

    feat: MainViewController, DetailViewController 생성

---
+++ AutoLayoutStudy/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  AutoLayoutStudy
+//
+//  Created by Jinho Choi on 2021/01/12.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
