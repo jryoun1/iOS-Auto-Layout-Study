42752e10e76b2c8f479e237e59e1ad6a06b888c3
zziro95 <opers7878@gmail.com>
2021-01-12 22:35:03 +0900

    feat: tablefooterView 개념을 이용해 최종 index 개수 표시

--- AutoLayoutStudy/MainViewController.swift
+++ AutoLayoutStudy/MainViewController.swift
@@ -11,11 +11,27 @@ class MainViewController: UIViewController {
     let sectionHeader: [String] = ["ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ"]
     let phoneNumberList: [[String]] = [["강아지", "개나리", "곰돌이"], ["나비", "나", "나침반", "나영석"], ["다치지말고", "두근두근"], ["라면", "러브", "라일락", "라디오", "레스고"], ["마음", "맴돌아", "매미", "메밀묵", "마차", "마징가", "물"]]
     
+    @IBOutlet weak var footerViewLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let searchController = UISearchController(searchResultsController: MainViewController())
+        searchController.searchBar.placeholder = "검색"
         self.navigationItem.searchController = searchController
         self.navigationItem.hidesSearchBarWhenScrolling = false
+        
+        footerViewLabel.text = "\(countPhoneNumberList())개의 연락처"
+        
+    }
+    
+    func countPhoneNumberList() -> Int {
+        var totalCount: Int = 0
+        for section in 0..<phoneNumberList.count {
+            for row in 0..<phoneNumberList[section].count {
+                totalCount += 1
+            }
+        }
+        return totalCount
     }
 }
 
