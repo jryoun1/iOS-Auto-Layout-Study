6cb12edf2b80042039c4f8ff929a2846aab061b2
GREENOVER <humains29@gmail.com>
2021-01-27 09:59:47 +0900

    feat: Step2 ColletionView 구현

--- AutoLayoutStudy/ViewController.swift
+++ AutoLayoutStudy/ViewController.swift
@@ -1,19 +1,38 @@
-//
-//  ViewController.swift
-//  AutoLayoutStudy
-//
-//  Created by Yeon on 2020/12/23.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
 
+    let images = ["1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RowCell", for: indexPath) as! CustomCell
+        cell.image.image = UIImage(named: images[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+      let cellWidth = (collectionView.frame.width / 3) - 1
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
 }
 
+class CustomCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+}
