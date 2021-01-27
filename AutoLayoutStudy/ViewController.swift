c54cf6eaf04a6eb0d7af7fb14a4186d842065df4
GREENOVER <humains29@gmail.com>
2021-01-27 10:05:15 +0900

    refactor: Week4 CollectionView 리팩토링

--- AutoLayoutStudy/ViewController.swift
+++ AutoLayoutStudy/ViewController.swift
@@ -1,25 +1,30 @@
 import UIKit
 
-class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController {
 
-    let images = ["1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png"]
+    let images = ["1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png","3.png","1.jpeg","2.png"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
 
+extension ViewController: UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RowCell", for: indexPath) as! CustomCell
+        
         cell.image.image = UIImage(named: images[indexPath.row])
+        
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-      let cellWidth = (collectionView.frame.width / 3) - 1
+        let cellWidth = (collectionView.frame.width / 3) - 1
+        
         return CGSize(width: cellWidth, height: cellWidth)
     }
 
@@ -30,7 +35,6 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 1
     }
-    
 }
 
 class CustomCell: UICollectionViewCell {
